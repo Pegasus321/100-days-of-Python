87490d01ab5ce6df0d2895c8529b1d07142db6f5
Ayush Agrawal <87698717+Pegasus321@users.noreply.github.com>
2022-08-03 23:59:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ") #5
+b = input("b: ") #100
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c=a
+a=b #a is equal to 100
+b=c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
